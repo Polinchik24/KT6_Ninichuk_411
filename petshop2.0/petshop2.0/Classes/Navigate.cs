731b5b93eceec5d3bbeca3b43987fa5ff8a83434
Коммерческие курсы <comcuru@teach.local>
2024-10-31 14:41:34 +0300

    3com

---
+++ petshop2.0/petshop2.0/Classes/Navigate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace petshop2._0.Classes
+{
+    class Navigate
+    {
+        public static Frame ActiveMain { get; set; }
+    }
+}
